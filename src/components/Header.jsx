85c20e40cd75949b659f8f8f12f9f4c5b483bbde
prathmesh soni <prathmeshsoni810@gmail.com>
2026-08-13 14:09:07 +0530

    My initial changese

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const appName ="TaskFlow"
+
+function Header(){
+    return(
+        <header className="app-header">
+            <h1>{appName}</h1>
+            <p>Manage your task easily.</p>
+            </header>
+       
+    )
+}
+export default Header
